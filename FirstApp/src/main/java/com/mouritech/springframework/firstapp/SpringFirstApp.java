64eb1afe9f8f6465d9103bb01076e140df309ed1
swapna <swapna-mori@gmail.com>
2022-02-02 15:55:30 +0530

    spring di example code added

--- FirstApp/src/main/java/com/mouritech/springframework/firstapp/SpringFirstApp.java
+++ FirstApp/src/main/java/com/mouritech/springframework/firstapp/SpringFirstApp.java
@@ -1,7 +1,9 @@
 package com.mouritech.springframework.firstapp;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class SpringFirstApp {
 
@@ -23,6 +25,26 @@ public class SpringFirstApp {
 		System.out.println(emp);
 		System.out.println(emp.getEmpId());
 		emp.setEmpId(456);
+		System.out.println(emp.getEmpId());
+		
+		System.out.println("===========================================");
+		@SuppressWarnings("deprecation")
+		XmlBeanFactory factory = 
+				new XmlBeanFactory
+					(new ClassPathResource("applicationContext.xml"));
+		
+		Quotation qu1 = (Quotation) factory.getBean("quotationService");
+		String quoteForDay1 = qu1.getQuote();
+		System.out.println(quoteForDay1);
+		
+		Greetings gr1 = (Greetings) factory.getBean("greetingsService");
+		String wish1 = gr1.getGreet();
+		System.out.println(wish1);
+		
+		Employee emp1= (Employee) factory.getBean("emp");
+		System.out.println(emp1);
+		System.out.println(emp.getEmpId());
+		emp.setEmpId(45622);
 		System.out.println(emp.getEmpId());
 
 	}
